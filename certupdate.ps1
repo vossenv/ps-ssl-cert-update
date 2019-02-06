4b01f04e500a4dd1e4e08e41124a32d1f1600004
Unknown <danimaejanssen@gmail.com>
2019-02-06 17:34:54 -0600

    Add certifcate printout

--- certupdate.ps1
+++ certupdate.ps1
@@ -22,40 +22,61 @@ function Add-NewSSLCert(){
         [String]$serviceAccount
     )
 
-    function Get-Certificate($thumbprint){
-        return Get-ChildItem -Path cert:\LocalMachine\My | Where-Object -FilterScript { $PSItem.ThumbPrint -eq $thumbprint; };    
+    function Show-Certificate($cert){
+
+        $san = (($cert.Extensions | Where-Object {$_.Oid.FriendlyName -eq "subject alternative name"}).Format(1)).Replace("DNS Name=","")
+        $san = $san.Split([Environment]::NewLine, [StringSplitOptions]::RemoveEmptyEntries)
+
+        Write-Host "`nCertificate Details`n--------------------"
+
+        Write-Host "Thumbprint: $($cert.Thumbprint)"
+        Write-Host "Subject: $($cert.Subject)"
+        Write-Host "Issuer: $($cert.IssuerName.Name)"
+        Write-Host "Issued: $($cert.NotBefore.ToString('MM/dd/yyyy'))"
+        Write-Host "Expires: $($cert.NotAfter.ToString('MM/dd/yyyy'))"
+        Write-Host "Subject Alternative Names:"
+        foreach ($s in $san) {Write-Host "  + $s"}
+        Write-Host
+
     }
 
     function Set-CertificatePermission ($pfxThumbPrint, $serviceAccount) {
 
-        $cert = Get-Certificate $pfxThumbPrint
+        $cert = Get-ChildItem -Path cert:\LocalMachine\My | Where-Object -FilterScript { $PSItem.ThumbPrint -eq $thumbprint }  
         $accessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $serviceAccount,"Read,FullControl","Allow";
         $keyFullPath = "$env:ProgramData\Microsoft\Crypto\RSA\MachineKeys\$($cert.PrivateKey.CspKeyContainerInfo.UniqueKeyContainerName)"
 
         $acl = Get-Acl -Path $keyFullPath
         $acl.SetAccessRule($accessRule)
         Set-Acl -Path $keyFullPath -AclObject $acl;
+        Write-Host "Successfully set permissions on private key for: $serviceAccount"
     }
 
     function Import-SSLCert($pfxFilePath, $pfxPass){    
         
+        Write-Host "Importing certificate to Local Machine\Personal store... "
         $flags = [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::MachineKeySet -bor `
                  [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::PersistKeySet -bor `
                  [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable;
         
         $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
         $cert.Import($pfxFilePath, $pfxPass, $flags)
+       
+        Show-Certificate $cert
                 
         $store = New-Object System.Security.Cryptography.X509Certificates.X509Store -ArgumentList "MY", LocalMachine
         $store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::"ReadWrite")
         $store.Add($cert)
         $store.Close()
-        return $cert
+
+        Write-Host "Successfully imported: $thumbprint"
+        return $cert.Thumbprint
     }
 
-    $cert = Import-SSLCert $pfxPath $pass    
-    if ($serviceAccount) { Set-CertificatePermission $cert.ThumbPrint $serviceAccount }
-    return Get-Certificate $cert.ThumbPrint
+    $thumbprint = Import-SSLCert $pfxPath $pass 
+    if ($serviceAccount) { Set-CertificatePermission $thumbPrint $serviceAccount }
+    Write-Host "Finished adding new certificate!"
+    return $thumbprint
 }
 
 function Convert-PEMtoPFX(){
@@ -66,10 +87,12 @@ function Convert-PEMtoPFX(){
         [String]$pass
     )
 
+    Write-Host "Converting $($certificate.Split("\")[-1]) to PFX format... "
     $key = (Resolve-Path $key).Path
     $certificate = (Resolve-Path $certificate).Path   
     $pfxPath = $certificate.Replace("pem","pfx")
     openssl pkcs12 -export -out $pfxPath -inkey $key -in $certificate -password pass:$pass
+    Write-Host "Conversion complete!"
     return $pfxPath
 }
 
@@ -86,8 +109,20 @@ function Set-NewSSLCert(){
    
 }
 
+Write-Host "Initiate SSL certificate update with the following parameters:"
+Write-Host "---------------------------------------------------------------"
+Write-Host "Certificate: $certificate"
+Write-Host "Private Key: $key"
+Write-Host "Service account: $serviceAccount"
+Write-Host "PFX password: $pass"
+Write-Host "`nBegin update`n-------------"
+
 $pfxPath = Convert-PEMtoPFX -k $key -c $certificate -p $pass
-$cert = Add-NewSSLCert -pfx $pfxPath -pass $pass -s $serviceAccount
+$thumbprint = Add-NewSSLCert -pfx $pfxPath -pass $pass -s $serviceAccount
+
+
+
+Write-Host ""
 
 
 
--- update.bat
+++ update.bat
@@ -1,2 +1,3 @@
 
-powershell.exe .\certupdate.ps1 -c C:\Resources\config\ssl_certificates\current\fullchain1.pem -k C:\Resources\config\ssl_certificates\current\privkey1.pem -s "Everyone"
+powershell.exe -File "C:\Resources\apps\SSL Cert Update\certupdate.ps1" -c C:\Resources\config\ssl_certificates\current\fullchain1.pem -k C:\Resources\config\ssl_certificates\current\privkey1.pem -s "Everyone"
+pause
