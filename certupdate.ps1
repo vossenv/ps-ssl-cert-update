12927f8ee871ff343e99408e734cdf1f5daf08df
jansseneda-adobe <danimaejanssen@gmail.com>
2019-02-07 08:20:03 -0600

    Add color

--- certupdate.ps1
+++ certupdate.ps1
@@ -5,14 +5,52 @@ param(
     [String]$pass="secret",
     [String]$serviceAccount
 );
+
+$ErrorActionPreference = "Stop"
+
+function Write-Color ($msg, $color) {
+    Write-ColorNB "$msg`n" $color
+}
+
+function Write-ColorNB ($msg, $color) {    
+    $color = if ($color) {$color} else {"white"}
+    Write-Host $msg -ForegroundColor $color -NoNewline
+}
+
+function Write-Field ($field, $value, $color) {
+    Write-ColorNB $field $color; Write-Color $value
+}
     
 $admin = ([Security.Principal.WindowsPrincipal] `
           [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
           [Security.Principal.WindowsBuiltInRole]::Administrator)
 
-if (-not $admin){Write-Host "`nPlease re-run as administrator... `n"; exit}
-if (-not $key){Write-Host "`nError: missing private key path (-key)...`n"; exit}
-if (-not $certificate){ Write-Host "`nMissing certificate path (-cert)...`n"; exit}
+if (-not $admin){Write-Color "`nPlease re-run as administrator... `n"; exit}
+if (-not $key){Write-Color "`nError: missing private key path (-key)...`n"; exit}
+if (-not $certificate){ Write-Color "`nMissing certificate path (-cert)...`n"; exit}
+
+
+
+function Write-Certificate($cert){
+
+    $san = (($cert.Extensions | Where-Object {$_.Oid.FriendlyName -eq "subject alternative name"}).Format(1)).Replace("DNS Name=","")
+    $san = $san.Split([Environment]::NewLine, [StringSplitOptions]::RemoveEmptyEntries)
+
+    Write-Color "`nCertificate Details`n--------------------" "green"
+    Write-Field "Thumbprint: " $cert.Thumbprint "darkyellow"
+    Write-Field "Subject: " $cert.Subject "darkyellow"
+    Write-Field "Issuer: " $cert.IssuerName.Name "darkyellow"
+    Write-Field "Issued: " $cert.NotBefore.ToString('MM/dd/yyyy') "darkyellow"
+    Write-Field "Expires: " $cert.NotAfter.ToString('MM/dd/yyyy') "darkyellow"
+    Write-Field "Subject Alternative Names: " "" "darkyellow"
+    foreach ($s in $san) {Write-ColorNB "  + " "blue"; Write-Color $s}
+    Write-Color ""
+
+}
+
+function Get-CertByPrint (){
+    return Get-ChildItem -Path cert:\LocalMachine\My | Where-Object -FilterScript { $PSItem.ThumbPrint -eq $thumbprint }  
+}
 
 function Add-NewSSLCert(){
 
@@ -22,39 +60,21 @@ function Add-NewSSLCert(){
         [String]$serviceAccount
     )
 
-    function Show-Certificate($cert){
-
-        $san = (($cert.Extensions | Where-Object {$_.Oid.FriendlyName -eq "subject alternative name"}).Format(1)).Replace("DNS Name=","")
-        $san = $san.Split([Environment]::NewLine, [StringSplitOptions]::RemoveEmptyEntries)
-
-        Write-Host "`nCertificate Details`n--------------------"
-
-        Write-Host "Thumbprint: $($cert.Thumbprint)"
-        Write-Host "Subject: $($cert.Subject)"
-        Write-Host "Issuer: $($cert.IssuerName.Name)"
-        Write-Host "Issued: $($cert.NotBefore.ToString('MM/dd/yyyy'))"
-        Write-Host "Expires: $($cert.NotAfter.ToString('MM/dd/yyyy'))"
-        Write-Host "Subject Alternative Names:"
-        foreach ($s in $san) {Write-Host "  + $s"}
-        Write-Host
-
-    }
-
     function Set-CertificatePermission ($pfxThumbPrint, $serviceAccount) {
 
-        $cert = Get-ChildItem -Path cert:\LocalMachine\My | Where-Object -FilterScript { $PSItem.ThumbPrint -eq $thumbprint }  
+        $cert = Get-CertByPrint $pfxThumbPrint
         $accessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $serviceAccount,"Read,FullControl","Allow";
         $keyFullPath = "$env:ProgramData\Microsoft\Crypto\RSA\MachineKeys\$($cert.PrivateKey.CspKeyContainerInfo.UniqueKeyContainerName)"
 
         $acl = Get-Acl -Path $keyFullPath
         $acl.SetAccessRule($accessRule)
         Set-Acl -Path $keyFullPath -AclObject $acl;
-        Write-Host "Successfully set permissions on private key for: $serviceAccount"
+        Write-Color "Successfully set permissions on private key for: $serviceAccount"
     }
 
     function Import-SSLCert($pfxFilePath, $pfxPass){    
         
-        Write-Host "Importing certificate to Local Machine\Personal store... "
+        Write-Color "Importing certificate to Local Machine\Personal store... "
         $flags = [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::MachineKeySet -bor `
                  [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::PersistKeySet -bor `
                  [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable;
@@ -62,20 +82,20 @@ function Add-NewSSLCert(){
         $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
         $cert.Import($pfxFilePath, $pfxPass, $flags)
        
-        Show-Certificate $cert
+        Write-Certificate $cert
                 
         $store = New-Object System.Security.Cryptography.X509Certificates.X509Store -ArgumentList "MY", LocalMachine
         $store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::"ReadWrite")
         $store.Add($cert)
         $store.Close()
 
-        Write-Host "Successfully imported: $thumbprint"
+        Write-Color "Successfully imported: $thumbprint"
         return $cert.Thumbprint
     }
 
     $thumbprint = Import-SSLCert $pfxPath $pass 
     if ($serviceAccount) { Set-CertificatePermission $thumbPrint $serviceAccount }
-    Write-Host "Finished adding new certificate!"
+    Write-Color "Finished adding new certificate!"
     return $thumbprint
 }
 
@@ -87,12 +107,12 @@ function Convert-PEMtoPFX(){
         [String]$pass
     )
 
-    Write-Host "Converting $($certificate.Split("\")[-1]) to PFX format... "
+    Write-Color "Converting $($certificate.Split("\")[-1]) to PFX format... "
     $key = (Resolve-Path $key).Path
     $certificate = (Resolve-Path $certificate).Path   
     $pfxPath = $certificate.Replace("pem","pfx")
     openssl pkcs12 -export -out $pfxPath -inkey $key -in $certificate -password pass:$pass
-    Write-Host "Conversion complete!"
+    Write-Color "Conversion complete!"
     return $pfxPath
 }
 
@@ -109,20 +129,20 @@ function Set-NewSSLCert(){
    
 }
 
-Write-Host "Initiate SSL certificate update with the following parameters:"
-Write-Host "---------------------------------------------------------------"
-Write-Host "Certificate: $certificate"
-Write-Host "Private Key: $key"
-Write-Host "Service account: $serviceAccount"
-Write-Host "PFX password: $pass"
-Write-Host "`nBegin update`n-------------"
+Write-Color "Initiate SSL certificate update with the following parameters:" "green"
+Write-Color "---------------------------------------------------------------" "green"
+Write-Color "Certificate: $certificate"
+Write-Color "Private Key: $key"
+Write-Color "Service account: $serviceAccount"
+Write-Color "PFX password: $pass"
+Write-Color "`nBegin update`n-------------" "green"
 
 $pfxPath = Convert-PEMtoPFX -k $key -c $certificate -p $pass
 $thumbprint = Add-NewSSLCert -pfx $pfxPath -pass $pass -s $serviceAccount
 
 
 
-Write-Host ""
+Write-Color ""
 
 
 
