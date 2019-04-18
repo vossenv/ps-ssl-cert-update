d5758bb7538a50945ac1cd344ed71910f33adf6b
jansseneda-adobe <danimaejanssen@gmail.com>
2019-04-17 21:58:44 -0500

    new name

--- update.bat
+++ update.bat
@@ -1,2 +1,2 @@
-powershell.exe -File "C:\Resources\apps\ssl-cert-update\certupdate.ps1" -c C:\Resources\config\ssl_certificates\current\seaofcarag.pfx
+powershell.exe -File "C:\Resources\apps\ssl-cert-update\certupdate.ps1" -c C:\Resources\config\ssl_certificates\current\carag.cloud.pfx
 pause
